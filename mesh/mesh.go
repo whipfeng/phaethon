f85283f39dbae528ec8f94aa7a9d5f2592c5b1da
= <=>
2026-09-14 18:01:15 +0800

    fix(mesh): add info log for hostIP packet delivery in HandleMeshFrame
    Helps trace DNS response path from remote nodes to local applications.

--- mesh/mesh.go
+++ mesh/mesh.go
@@ -282,11 +282,7 @@ func (m *MeshManager) isLocalVIP(ip net.IP) bool {
 	if m.vip == nil {
 		return false
 	}
-	result := ip.Equal(m.vip)
-	if !result {
-		util.LogInfo("[MESH] isLocalVIP: ip=%s vip=%s equal=%v", ip, m.vip, result)
-	}
-	return result
+	return ip.Equal(m.vip)
 }
 
 // getHostIP returns the hostIP (.2) address for this node's subnet.
@@ -318,11 +314,7 @@ func (m *MeshManager) getGIP() net.IP {
 // isLocalHostIP checks if the IP is the local hostIP (.2) address.
 func (m *MeshManager) isLocalHostIP(ip net.IP) bool {
 	hostIP := m.getHostIP()
-	if hostIP != nil && ip.Equal(hostIP) {
-		return true
-	}
-	util.LogInfo("[MESH] isLocalHostIP: ip=%s hostIP=%v equal=%v", ip, hostIP, hostIP != nil && ip.Equal(hostIP))
-	return false
+	return hostIP != nil && ip.Equal(hostIP)
 }
 
 // isLocalGIP checks if the IP is the local GIP (.3) address.
